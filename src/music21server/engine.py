8e4d4b93d36fbeff3be9a2cd5d5d8409805737ba
Koya <koya0306@outlook.jp>
2020-11-03 21:46:05 +0900

    Engineにデバッグモードを追加

--- src/music21server/engine.py
+++ src/music21server/engine.py
@@ -1,17 +1,28 @@
 from music21 import chord
 
 
-class Engine:
+class Engine(object):
     def __init__(self):
         self.__is_active = True
+        self.__debug = False
 
     @property
     def is_active(self) -> bool:
         return self.__is_active
 
+    @property
+    def debug(self) -> bool:
+        return self.__debug
+
+    @debug.setter
+    def debug(self, is_debbug: bool):
+        self.__debug = is_debbug
+
     def execute(self, value):
         if 'command' not in value:
             raise 'command is not specified'
+        if self.debug:
+            print(f'- {value}')
         # コマンドごとに処理を分岐する
         command = value['command']
         if command == 'chord_name':
